d789dc2036fff909f2e2e88ccb72e290da3db0c6
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-06 10:37:24 -0700

    Add files via upload

---
+++ if-statements.js
@@ -0,0 +1,6 @@
+const fruit = 'dragonfruit'
+if (fruit.length > 5) {
+    console.log('The fruit name has more than five characters.');
+} else {
+    console.log('The fruit name has five characters or less.');
+}
